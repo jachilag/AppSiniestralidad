b2cc6c5c0e7d3eb6fd75e694e28bdf6235309908
jachilag <jachilag@gmail.com>
2022-10-31 02:32:55 -0500

    jerarquia carpetas y modificacion de dependencias

--- AppSiniestralidadKotlin/app/src/main/java/com/example/appsiniestralidadkotlin/View/ui/activities/MainActivity.kt
+++ AppSiniestralidadKotlin/app/src/main/java/com/example/appsiniestralidadkotlin/View/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.appsiniestralidadkotlin
+package com.example.appsiniestralidadkotlin.View.ui.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appsiniestralidadkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
