c01e8581d99406a5c9d06f26eabf7f2208bd7ac0
jachilag <jachilag@gmail.com>
2022-10-31 17:03:01 -0500

    animacion y login funcionando

---
+++ AppSiniestralidadKotlin/app/src/main/java/com/example/appsiniestralidadkotlin/view/ui/activities/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.appsiniestralidadkotlin.view.ui.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appsiniestralidadkotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var handler: Handler
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // asociar activity con el layout
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // configuracion para que funcione la animacion
+        binding.animationView.playAnimation()
+
+        //transicion animacion  y la siguiente activity
+        handler = Handler(Looper.myLooper()!!)
+        handler.postDelayed({
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+    }
+}
